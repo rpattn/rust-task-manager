1fbd6e94ccb4f07186dc37fc8463e0e931f670d0
rpattn <robpatton04@gmail.com>
2026-04-01 23:50:44 +0100

    tidy up remove feature

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,8 @@ mod tasks;
 mod store;
 
 use tasks::Manager;
+use tasks::manager::RemoveBy;
 use tasks::Task;
-
-use crate::tasks::manager::RemoveBy;
 
 const TASKS_FILENAME: &'static str = "out/tasks.json";
 
@@ -15,13 +14,11 @@ fn main() {
     let task = Task::default();
 
     manager.add(task.clone());
-    manager.add(task.clone());
-    manager.add(task.clone());
-
     if let Some(task) = manager.get(1).cloned() {
         manager.remove(task);
     };
 
+    manager.add(task.clone());
     manager.remove(RemoveBy::Last);
 
     println!("Tasks: {:?}", manager);
